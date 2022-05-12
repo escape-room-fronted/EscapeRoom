c73baa7112efa5cc90855659cccab88440e619c9
daniela <moreno.tobar.daniela@gmail.com>
2022-05-12 10:23:22 -0500

    buttons

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,14 @@ function App() {
 
   return (
     <div className="App">
-    <div className='min-h-screen bg-dark text-white'>
+    <div className='min-h-screen  text-white'>
       <h1 className='font-monserrat bg-yellow text-dark text-4xl'>Hola mundo</h1>
       <h1 className='font-hind bg-light text-4xl'>Hola mundo</h1>
       <h1 className='font-hind bg-gray text-4xl'>Hola mundo</h1>
+      <div className='pt-8'>
+      <button className='btn-yellow'>Ver más</button>
+      <button className='btn-dark'>Enviar mensaje</button>
+      </div>
     </div>
   </div>
   )
