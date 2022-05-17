363712a9a808883b16ad6afed2efc8f2a1a4c2b7
christian lazo <kenishi.cristianxd@gmail.com>
2022-05-17 15:26:03 -0500

    mege christian02

--- src/components/molecules/FormLogin.jsx
+++ src/components/molecules/FormLogin.jsx
@@ -16,8 +16,8 @@ const FormLogin = () => {
       const response = await axios.post(
         LOGIN_URL,
         JSON.stringify({
-          email: "admin@educamas",
-          password: "1234",
+          email: formValues.email,
+          password: formValues.password,
         }),
         {
           headers: { "Content-Type": "application/json" },
@@ -26,7 +26,7 @@ const FormLogin = () => {
       const accesToken = response.data.token;
       console.log(response);
       setAuth({
-        email: "admin@educamas.com",
+        email: formValues.email,
         accesToken,
       });
       navigate("/logic-room");
