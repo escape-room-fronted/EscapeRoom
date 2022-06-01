2afcb51030bc28db0f12972c19061da4f88deea0
christian lazo <kenishi.cristianxd@gmail.com>
2022-06-01 16:24:50 -0500

    Adding Change State View Room

--- src/components/molecules/Questions.jsx
+++ src/components/molecules/Questions.jsx
@@ -1,38 +1,30 @@
 import React, { useEffect, useState } from "react";
-import { randomAnswer } from '../../Helpers/helpers';
 
-const Questions = ({ handleViewRoom, dataQuestions }) => {
-
-
+const Questions = ({ handleNumberQuestions, dataQuestions }) => {
   const [dataAnswers, setDataAnswers] = useState();
 
-
-  console.log(dataAnswers);
   useEffect(() => {
     console.log("ejecutando");
     let data = [];
-    if (dataQuestions && data.length === 0) {
-      let random = randomAnswer();
+    if (dataQuestions) {
       data = dataQuestions.incorrect_answers;
       data = [...data, dataQuestions.correct_answer];
       data.sort();
       setDataAnswers(data);
     }
-  }, []); 
-
+  }, [dataQuestions]);
 
   return (
     dataAnswers && (
       <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 gap-6 font-hind text-lg">
-        {/* <button
-          onClick={() => handleViewRoom()}
-          className="cursor-pointer card__animate py-4 px-6 text-white hover:text-dark bg-gray hover:bg-yellow rounded-lg"
-        >
-          <a>Answer 1</a>
-        </button> */}
-
         {dataAnswers.map((item) => (
-          <button key={item} className="cursor-pointer card__animate py-4 px-6 text-white hover:text-dark bg-gray hover:bg-yellow rounded-lg">
+          <button
+            key={item}
+            className="cursor-pointer card__animate py-4 px-6 text-white hover:text-dark bg-gray hover:bg-yellow rounded-lg"
+            onClick={() => {
+              handleNumberQuestions();
+            }}
+          >
             {item}
           </button>
         ))}
--- src/components/templates/LogicPanel.jsx
+++ src/components/templates/LogicPanel.jsx
@@ -10,22 +10,32 @@ const LogicPanel = ({ handleViewRoom }) => {
 
   const [dataQuestions, setDataQuestions] = useState();
 
+  const handleNumberQuestions = () => {
+    setNumberQuestions((prev) => prev + 1);
+    if (numberQuestions === dataQuestions.length - 1) {
+      handleViewRoom();
+    }
+  };
+
   useEffect(() => {
     setDataQuestions(questions);
     console.log("ejecutando");
   }, [questions]);
-  
 
   return (
     dataQuestions && (
       <div className=" p-4">
         <div className="">
           <div className="flex justify-center">
-            <CardLogic numberQuestions={numberQuestions} dataQuestions={dataQuestions[numberQuestions]} />
+            <CardLogic
+              numberQuestions={numberQuestions}
+              dataQuestions={dataQuestions[numberQuestions]}
+            />
           </div>
           <div className="flex justify-center pt-10">
             <Questions
               handleViewRoom={handleViewRoom}
+              handleNumberQuestions={handleNumberQuestions}
               dataQuestions={dataQuestions[numberQuestions]}
             />
           </div>
