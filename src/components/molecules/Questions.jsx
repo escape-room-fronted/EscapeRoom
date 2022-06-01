19d091634afafd2342d211e222db1fcbe35d86f6
christian lazo <kenishi.cristianxd@gmail.com>
2022-06-01 12:00:32 -0500

    Adding Logic 20%

--- src/components/molecules/Questions.jsx
+++ src/components/molecules/Questions.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const Questions = ({handleViewRoom}) => {
+const Questions = ({ handleViewRoom, dataQuestions }) => {
+  console.log(dataQuestions);
   return (
-    <div className='grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 gap-6 font-hind text-lg'>
+    dataQuestions && (
+      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 gap-6 font-hind text-lg">
+        {/* <button
+          onClick={() => handleViewRoom()}
+          className="cursor-pointer card__animate py-4 px-6 text-white hover:text-dark bg-gray hover:bg-yellow rounded-lg"
+        >
+          <a>Answer 1</a>
+        </button> */}
 
-        <button onClick={() => handleViewRoom()}  className='cursor-pointer card__animate py-4 px-6 text-white hover:text-dark bg-gray hover:bg-yellow rounded-lg'>
-            <a>Answer 1</a>
-        </button>
+        {dataQuestions.incorrect_answers.map((item) => (
+          <button className="cursor-pointer card__animate py-4 px-6 text-white hover:text-dark bg-gray hover:bg-yellow rounded-lg">
+            {item}
+          </button>
+        ))}
+      </div>
+    )
+  );
+};
 
-        <div className='card__animate py-4 px-6 text-white hover:text-dark bg-gray hover:bg-yellow rounded-lg'>
-            <a>Answer 1</a>
-        </div>
-
-        <div className='card__animate py-4 px-6 text-white hover:text-dark bg-gray hover:bg-yellow rounded-lg'>
-            <a>Answer 1</a>
-        </div>
-
-        <div className='card__animate py-4 px-6 text-white hover:text-dark bg-gray hover:bg-yellow rounded-lg'>
-            <a>Answer 1</a>
-        </div>
-
-    </div>
-  )
-}
-
-export default Questions
+export default Questions;
--- src/components/templates/LogicPanel.jsx
+++ src/components/templates/LogicPanel.jsx
@@ -1,40 +1,35 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import CardLogic from "../molecules/CardLogic";
 import Questions from "../molecules/Questions";
 import useAuth from "../../hooks/useAuth";
 
-
-
-const LogicPanel = ({handleViewRoom}) => {
-
+const LogicPanel = ({ handleViewRoom }) => {
   const [numberQuestions, setNumberQuestions] = useState(0);
-  
-  const {questions} = useAuth();
+
+  const { questions } = useAuth();
 
   const [dataQuestions, setDataQuestions] = useState();
 
   useEffect(() => {
-    setDataQuestions(questions)
+    setDataQuestions(questions);
   }, [questions]);
 
-  
-  return dataQuestions &&  (
-
-
-          <div className=" p-4">
-            <div className="">
-              <div className="flex justify-center">
-                <CardLogic 
-                dataQuestions={dataQuestions[numberQuestions]} 
-                />
-              </div>
-              <div className="flex justify-center pt-10">
-                <Questions handleViewRoom={handleViewRoom} 
-                />
-              </div>
-            </div>
+  return (
+    dataQuestions && (
+      <div className=" p-4">
+        <div className="">
+          <div className="flex justify-center">
+            <CardLogic dataQuestions={dataQuestions[numberQuestions]} />
           </div>
-
+          <div className="flex justify-center pt-10">
+            <Questions
+              handleViewRoom={handleViewRoom}
+              dataQuestions={dataQuestions[numberQuestions]}
+            />
+          </div>
+        </div>
+      </div>
+    )
   );
 };
 
