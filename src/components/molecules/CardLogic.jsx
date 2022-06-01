655d4e43c972cdccb97d72d34600156afc97c515
christian lazo <kenishi.cristianxd@gmail.com>
2022-06-01 11:51:25 -0500

    update

--- src/components/molecules/CardLogic.jsx
+++ src/components/molecules/CardLogic.jsx
@@ -2,32 +2,34 @@ import React from "react";
 import ModalClueUser from "./ModalClueUser";
 import { FcIdea } from "react-icons/fc";
 
-const CardLogic = ({questions}) => {
+const CardLogic = ({ dataQuestion }) => {
   const [showModal, setShowModal] = React.useState(false);
-  console.log(questions);
+  console.log(dataQuestion);
   return (
-    <>
-      <div className="pt-2">
-        <div
-          href="#"
-          className="block card__animate p-8 max-w-lg bg-gray rounded-lg"
-        >
-          <div className="flex justify-between">
-            <h5 className="font-hind text-2xl font-bold tracking-tight text-white">
-              Question 1/10
-            </h5>
-            <button type="button" onClick={() => setShowModal(true)}>
-              <FcIdea className="text-2xl" />
-            </button>
+    dataQuestion && (
+      <>
+        <div className="pt-2">
+          <div
+            href="#"
+            className="block card__animate p-8 max-w-lg bg-gray rounded-lg"
+          >
+            <div className="flex justify-between">
+              <h5 className="font-hind text-2xl font-bold tracking-tight text-white">
+                Pregunta {dataQuestion.question_number}
+              </h5>
+              <button type="button" onClick={() => setShowModal(true)}>
+                <FcIdea className="text-2xl" />
+              </button>
+            </div>
+            <p className="pt-4 font-normal font-hind text-white">
+              {dataQuestion.question}
+            </p>
           </div>
-          <p className="pt-4 font-normal font-hind text-white">
-            {/* {questions.question} */}
-          </p>
         </div>
-      </div>
 
-      <ModalClueUser state={showModal} setState={setShowModal} />
-    </>
+        <ModalClueUser state={showModal} setState={setShowModal} />
+      </>
+    )
   );
 };
 
--- src/components/molecules/Questions.jsx
+++ src/components/molecules/Questions.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const Questions = ({handleViewRoom}) => {
+const Questions = ({
+  handleViewRoom,
+  dataAnswers,
+  numberQuestion,
+  setNumberQuestion,
+}) => {
+  //   const answers = [
+  //     ...dataAnswers.incorrect_answers,
+  //     dataAnswers.correct_answer,
+  //   ];
   return (
-    <div className='grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 gap-6 font-hind text-lg'>
+    dataAnswers && (
+      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 gap-6 font-hind text-lg">
+        {/* {answers.map((item) => (
+          <button
+            className="cursor-pointer card__animate py-4 px-6 text-white hover:text-dark bg-gray hover:bg-yellow rounded-lg"
+            onClick={() => setNumberQuestion((prev) => prev + 1)}
+          >
+            {item}
+          </button>
+        ))} */}
+      </div>
+    )
+  );
+};
 
-        <button onClick={() => handleViewRoom()}  className='cursor-pointer card__animate py-4 px-6 text-white hover:text-dark bg-gray hover:bg-yellow rounded-lg'>
-            <a>Answer 1</a>
-        </button>
-
-        <div className='card__animate py-4 px-6 text-white hover:text-dark bg-gray hover:bg-yellow rounded-lg'>
-            <a>Answer 1</a>
-        </div>
-
-        <div className='card__animate py-4 px-6 text-white hover:text-dark bg-gray hover:bg-yellow rounded-lg'>
-            <a>Answer 1</a>
-        </div>
-
-        <div className='card__animate py-4 px-6 text-white hover:text-dark bg-gray hover:bg-yellow rounded-lg'>
-            <a>Answer 1</a>
-        </div>
-
-    </div>
-  )
-}
-
-export default Questions
+export default Questions;
